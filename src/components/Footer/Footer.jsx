e48a1e0c461975fa0f5f9fbca34e7e64443c8514
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-06-17 00:49:48 +0430

    create footer components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+import { AiOutlinePlus } from 'react-icons/ai';
+
+const Footer = () => {
+	return (
+		<footer>
+			<button>
+				<AiOutlinePlus />
+			</button>
+		</footer>
+	);
+};
+
+export default Footer;
