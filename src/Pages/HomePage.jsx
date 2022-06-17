4e8cc37d5daeb18043724c324e969176cdc9a317
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-06-17 20:51:31 +0430

    updated income and expense value

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import OverViewComponent from '../components/OverViewComponent/OverViewComponent';
 import TransActionsList from '../components/TransActions/TransActionsList';
 import Layout from './../Layout/Layout';
@@ -7,6 +7,20 @@ const HomePage = () => {
 	const [income, setIncome] = useState(0);
 	const [expense, setExpense] = useState(0);
 	const [transActions, setTransActions] = useState([]);
+
+	useEffect(() => {
+		let inc = 0;
+		let exp = 0;
+
+		transActions.forEach((transAction) =>
+			transAction.type === 'expense'
+				? (exp += parseFloat(transAction.amount))
+				: (inc += parseFloat(transAction.amount))
+		);
+
+		setExpense(exp)
+		setIncome(inc)
+	}, [transActions]);
 
 	const addTransActionHandler = (formValue) => {
 		const transActionData = { ...formValue, id: Math.floor(Math.random() * 1000) };
