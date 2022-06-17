77db25b1df7d3888e1b23aed14e3704c57cc4bb4
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-06-17 17:25:47 +0430

    add Balance section to show income and expense difference

--- src/components/OverViewComponent/OverViewComponent.jsx
+++ src/components/OverViewComponent/OverViewComponent.jsx
@@ -1,16 +1,23 @@
 import './OverViewComponent.css';
 
-const OverViewComponent = ({income, expense}) => {
+const OverViewComponent = ({ income, expense }) => {
 	return (
 		<section className='overView-container'>
-			<section className='income-container'>
-				<h4>InCome</h4>
-                <h5>$ {income}</h5>
-			</section>
+			<section className='income-expense-container'>
+				<section className='income-container'>
+					<h4>InCome</h4>
+					<h5>$ {income}</h5>
+				</section>
 
-			<section className='expense-container'>
-				<h4>Expense</h4>
-                <h5>$ {expense}</h5>
+				<section className='balance-container'>
+					<h4>Balance</h4>
+					<h5>$ {income - expense}</h5>
+				</section>
+
+				<section className='expense-container'>
+					<h4>Expense</h4>
+					<h5>$ {expense}</h5>
+				</section>
 			</section>
 		</section>
 	);
