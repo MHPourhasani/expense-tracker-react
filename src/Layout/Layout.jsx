7524e7cb50576c2571a98b781fa4096bc0fab474
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-06-17 17:51:06 +0430

    delete Footer component

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,10 @@
 import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
 
 const Layout = ({ children }) => {
 	return (
 		<>
 			<Header />
 			{children}
-			<Footer />
 		</>
 	);
 };
