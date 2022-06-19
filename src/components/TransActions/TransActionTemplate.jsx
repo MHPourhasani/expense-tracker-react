e07b3f556bb5493cd7fe7881e2bd6b560c16ffad
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-06-19 16:07:56 +0430

    add description paragraph and change some styles

--- src/components/TransActions/TransActionTemplate.jsx
+++ src/components/TransActions/TransActionTemplate.jsx
@@ -4,21 +4,26 @@ import styles from './TransActionsList.module.css';
 const TransActionTemplate = ({ transAction, deleteTransActionHandler }) => {
 	return (
 		<section
-			className={`${styles.transActionList} ${
+			className={`${styles.transAction_template} ${
 				transAction.type === 'income'
 					? styles.income_transAction
 					: styles.expense_transAction
 			}`}>
-			<p className={styles.transAction_title}>{transAction.title}</p>
+			<section className={styles.transAction_template_section1}>
+				<p className={styles.transAction_title}>{transAction.title}</p>
 
-			<section className={styles.amount_btns_container}>
-				<p className={styles.transAction_title}>$ {transAction.amount}</p>
-				<button
-					onClick={() => deleteTransActionHandler(transAction.id)}
-					className={styles.trash_btn}>
-					<BsTrash />
-				</button>
+				<section className={styles.amount_btns_container}>
+					<p className={styles.transAction_title}>$ {transAction.amount}</p>
+					<button
+						onClick={() => deleteTransActionHandler(transAction.id)}
+						className={styles.trash_btn}>
+						<BsTrash />
+					</button>
+				</section>
 			</section>
+
+			<p className={styles.transAction_desc}>{transAction.desc}</p>
+
 		</section>
 	);
 };
