ec1884371fe20edc870cb95105871d431c7e856c
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-06-21 00:23:56 +0430

    add flex styles to Layout component and responsive for all devices

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,12 @@
+import './Layout.css';
 import Header from '../components/Header/Header';
 
 const Layout = ({ children }) => {
 	return (
-		<>
+		<section className='layout'>
 			<Header />
 			{children}
-		</>
+		</section>
 	);
 };
 
