2df08560dd1928ffe555d8d7fe024a0828bcc0a7
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-06-21 00:30:15 +0430

    responsive OverViewComponent for all devices

--- src/components/OverViewComponent/OverViewComponent.jsx
+++ src/components/OverViewComponent/OverViewComponent.jsx
@@ -3,7 +3,7 @@ import './OverViewComponent.css';
 const OverViewComponent = ({ income, expense }) => {
 	return (
 		<section className='overView-container'>
-			<section className='income-expense-container'>
+			<section className='income-balance-expense-container'>
 				<section className='income-container'>
 					<h4>InCome</h4>
 					<h5>$ {income}</h5>
