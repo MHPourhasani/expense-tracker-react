eb14adf0d7b09b4e023e3ccffef1c1564f9823cc
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-10-11 00:02:55 +0330

    improving some styles and delete extra elements in OverView component

--- src/components/OverViewComponent/OverViewComponent.jsx
+++ src/components/OverViewComponent/OverViewComponent.jsx
@@ -3,22 +3,20 @@ import './OverViewComponent.css';
 const OverViewComponent = ({ income, expense }) => {
 	return (
 		<section className='overView-container'>
-			<section className='income-balance-expense-container'>
-				<section className='income-container'>
-					<h4>InCome</h4>
-					<h5>$ {income}</h5>
-				</section>
+			<div className='income-container'>
+				<h4>InCome</h4>
+				<h5>$ {income}</h5>
+			</div>
 
-				<section className='balance-container'>
-					<h4>Balance</h4>
-					<h5>$ {income - expense}</h5>
-				</section>
+			<div className='balance-container'>
+				<h4>Balance</h4>
+				<h5>$ {income - expense}</h5>
+			</div>
 
-				<section className='expense-container'>
-					<h4>Expense</h4>
-					<h5>$ {expense}</h5>
-				</section>
-			</section>
+			<div className='expense-container'>
+				<h4>Expense</h4>
+				<h5>$ {expense}</h5>
+			</div>
 		</section>
 	);
 };
